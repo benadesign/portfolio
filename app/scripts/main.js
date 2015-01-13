a767321127f34d80913c365a7e334ab6a7f73b6c
Benjamin Bissonnette <benjamin.bissonnette@gmail.com>
2015-01-13 13:25:50 -0600

    updates

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,3 +1,5 @@
+var currentPage = 'home';
+
 $('.js-link').on('click', function(e){
 	e.preventDefault();
 	switch(e.target.href.split('/').pop()){
@@ -13,26 +15,48 @@ $('.js-link').on('click', function(e){
 });
 
 $('.left .back').on('click', function(){
+	currentPage = 'home';	
 	$('.page').removeClass('active');	
 	aboutTl.reverse();
 });
 
+$('.right .back').on('click', function(){
+	currentPage = 'home';	
+	$('.page').removeClass('active');	
+	worksTL.reverse();
+});
+
+
+
 var aboutTl = new TimelineMax({paused : true});
-aboutTl.add( TweenLite.to('.left', 0.25, {width : '100%', ease : Quad.easeOut, onComplete : function(){ TweenLite.set('.right', {css : {display : 'none'}}) } } ) );
+aboutTl.add( TweenLite.to(['.left nav', '.left h1'], 0.1, {opacity : 0}));
+aboutTl.add( TweenLite.to('.left', 0.25, {width : '100%', ease : Quad.easeOut}));
 aboutTl.add( TweenLite.to('#page-home', 0.35, {xPercent : 50, ease : Linear.easeNone} ) );
-aboutTl.add( TweenLite.to(['#page-home'], 0.35, {xPercent : 80, ease : Quint.easeOut} ) );
-aboutTl.add( TweenLite.to(['#reactor'], 0.35, { css : {left : '80%'} , ease : Quint.easeOut} ), '-=0.35' );
+aboutTl.add( TweenLite.to(['#page-home'], 0.25, {xPercent : 80, ease : Linear.easeNone} ) );
+aboutTl.add( TweenLite.to(['#reactor'], 0.25, { css : {left : '80%'} , ease : Linear.easeNone} ), '-=0.25' );
+aboutTl.add( TweenLite.to('.left .back', 0.5, {opacity: 1, ease : Quint.easeOut} ), '-=0.25' );
 
 function about(){
+	currentPage = 'about';
 	$('.page').removeClass('active');
-	$('#page-about').addClass('active');
-	TweenLite.set('.left', {css : {'z-index' : 100}});
+	$('#page-about').addClass('active');	
 	
 	aboutTl.restart();
 }
 
+
+var worksTL = new TimelineMax({paused : true});
+worksTL.add( TweenLite.to(['.right nav', '.right h1'], 0.1, {opacity : 0}));
+worksTL.add( TweenLite.to('.right', 0.25, {width : '100%', ease : Quad.easeOut}));
+worksTL.add( TweenLite.to('#page-home', 0.35, {xPercent : -50, ease : Linear.easeNone} ) );
+worksTL.add( TweenLite.to(['#page-home'], 0.25, {xPercent : -80, ease : Linear.easeNone} ) );
+worksTL.add( TweenLite.to(['#reactor'], 0.25, { css : {left : '20%'} , ease : Linear.easeNone} ), '-=0.25' );
+worksTL.add( TweenLite.to('.right .back', 0.5, {opacity: 1, ease : Quint.easeOut} ), '-=0.25' );
+
 function works(){
-		$('.page').removeClass('active');
-	$('#page-works').addClass('active');	
-	console.log('works page');
+	currentPage = 'works';
+	$('.page').removeClass('active');
+	$('#page-works').addClass('active');		
+	
+	worksTL.restart();
 }
--- gulpfile.js
+++ gulpfile.js
@@ -11,7 +11,7 @@ gulp.task('styles', function () {
       style: 'expanded',
       precision: 10
     }))
-    .pipe($.autoprefixer({browsers: ['last 1 version']}))
+    .pipe($.autoprefixer({browsers: ['last 2 version']}))
     .pipe(gulp.dest('.tmp/styles'));
 });
 
